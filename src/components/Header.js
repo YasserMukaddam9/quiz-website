afd1fed0462b0ff79fef25bd9fe9306eaa120a0f
YasserMukaddam9 <yassermuk2012@gmail.com>
2023-07-26 20:28:12 +0530

    commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="app-header">
+      <img src="logo.png" alt="Football logo" />
+      <h1>Football Quiz</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/StartScreen.js
@@ -0,0 +1,20 @@
+import { useQuiz } from "../contexts/QuizContext";
+
+function StartScreen() {
+  const { numQuestions, dispatch } = useQuiz();
+
+  return (
+    <div className="start">
+      <h2>Welcome to Football Quiz!</h2>
+      <h3>{numQuestions} questions to test your football knowledge</h3>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "start" })}
+      >
+        Let's start
+      </button>
+    </div>
+  );
+}
+
+export default StartScreen;
